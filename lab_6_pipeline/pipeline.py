eb8e0d547745e1aecb22045c8ab7cf3c97553f88
Your Name <tusyakulak.4@gmail.com>
2024-05-18 20:11:32 +0300

    fighting with flake8

--- lab_6_pipeline/pipeline.py
+++ lab_6_pipeline/pipeline.py
@@ -4,10 +4,10 @@ Pipeline for CONLL-U formatting.
 # pylint: disable=too-few-public-methods, unused-import, undefined-variable, too-many-nested-blocks
 import pathlib
 
-import spacy_udpipe
 from networkx import DiGraph
+import spacy_udpipe
 
-from core_utils.article.article import (Article, ArtifactType, get_article_id_from_filepath)
+from core_utils.article.article import Article, ArtifactType, get_article_id_from_filepath
 from core_utils.article.io import from_raw, to_cleaned
 from core_utils.constants import ASSETS_PATH, UDPIPE_MODEL_PATH
 from core_utils.pipeline import (AbstractCoNLLUAnalyzer, CoNLLUDocument, LibraryWrapper,
